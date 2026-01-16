88b297a1e6a7bd63213b2e15f959ca6e9cdac1b1
bwoc Bảo <24520155@gm.uit.edu.vn>
2026-01-16 16:06:00 +0700

    first commit

---
+++ auth-service/src/main/java/com/r2s/auth/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.r2s.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Auth Service";
+    }
+}
