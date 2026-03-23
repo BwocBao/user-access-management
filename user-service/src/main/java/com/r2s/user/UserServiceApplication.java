3d09da222e3aa4b2733659ff828b3c0acd774f97
bwoc Bảo <24520155@gm.uit.edu.vn>
2026-03-23 17:57:34 +0700

    rabbitMQ outbox pattern v1

--- user-service/src/main/java/com/r2s/user/UserServiceApplication.java
+++ user-service/src/main/java/com/r2s/user/UserServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.r2s.user", "com.r2s.core"})
 //@EnableJpaRepositories(basePackages = "com.r2s.core.repository")
 //@EntityScan(basePackages = "com.r2s.core.entity")
